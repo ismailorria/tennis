b416bb9d91b7428541f3067ebe30016e29a24297
ismailorria <ismailorria@gmail.com>
2015-04-19 08:23:50 +0430

    Create detail.php
    this is a view detail file

---
+++ detail.php
@@ -0,0 +1,38 @@
+<?php
+echo "playerno:<br>";
+echo $player['playerno'].'<br>';
+
+echo "name:<br>";
+echo $player['name'].'<br>';
+
+echo "initials:<br>";
+echo $player['initials'].'<br>';
+
+echo "birth of date:<br>";
+echo $player['birth_date'].'<br>';
+
+echo "sex:<br>";
+echo $player['sex'].'<br>';
+
+echo "joined<br>";
+echo $player['joined'].'<br>';
+
+echo "street:<br>";
+echo $player['street'].'<br>';
+
+echo "house number<br>";
+echo $player['houseno'].'<br>';
+
+echo "postcode:<br>";
+echo $player['postcode'].'<br>';
+
+echo "town:<br>";
+echo $player['town'].'<br>';
+
+echo "phone number:<br>";
+echo $player['phoneno'].'<br>';
+
+echo "league number<br>";
+echo $player['leagueno'].'<br>';
+
+?>
